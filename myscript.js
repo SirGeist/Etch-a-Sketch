79343fb40efc6f430dece2243236ef67cbcb3c24
SirGeist <trinh1234533@yahoo.com>
2024-03-09 17:07:47 -0600

    added comments; hovering over the grids changes its color

--- myscript.js
+++ myscript.js
@@ -1,10 +1,24 @@
-const gridContainer = document.querySelector(".grid-container");
+// Main container
+const gridContainer = document.querySelectorAll(".grid-container");
+let size = 16;
 
-const fragment = document.createDocumentFragment();
-for (let i = 0; i < 16 * 16; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("grid-cell");
-  fragment.appendChild(cell);
-}
+gridContainer.forEach((gridContainer) => {
+  // Use a fragment to efficiently append all of the cells
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
 
-gridContainer.appendChild(fragment);
+    // Change grid cell background color when mouse hovers over it
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "blue";
+    });
+    fragment.appendChild(cell);
+  }
+
+  // Append the fragment to our container
+  gridContainer.appendChild(fragment);
+
+  // Creating Grid style class for the main container
+  gridContainer.classList.add("styled-grid-container");
+});

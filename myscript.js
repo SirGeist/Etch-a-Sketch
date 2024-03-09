37c47da6f7c6dc3fcff4dc4a1054a2506a4dd84a
SirGeist <trinh1234533@yahoo.com>
2024-03-09 17:33:56 -0600

    Generate a new grid; added error handling

--- myscript.js
+++ myscript.js
@@ -3,8 +3,12 @@ function generateGrid(size) {
   const gridContainer = document.querySelectorAll(".grid-container");
 
   gridContainer.forEach((gridContainer) => {
+    // Emptying the existing grid
+    gridContainer.innerHTML = "";
     // Use a fragment to efficiently append all of the cells
     const fragment = document.createDocumentFragment();
+
+    // Loop size * size
     for (let i = 0; i < size * size; i++) {
       const cell = document.createElement("div");
       cell.classList.add("grid-cell");
@@ -23,3 +27,18 @@ function generateGrid(size) {
     gridContainer.classList.add("styled-grid-container");
   });
 }
+
+document.getElementById("generate-grid-btn").addEventListener("click", () => {
+  // Retrieving the string input from the html
+  const gridSizeInput = document.getElementById("grid-size");
+  // Converting the string to int
+  const gridSize = parseInt(gridSizeInput.value);
+
+  // If grid is out of bounds, alert error and try again. Otherwise generate grid
+  // with the size
+  if (gridSize > 100 || gridSize < 1) {
+    alert("Error: Grid size must be between 1 and 100!");
+  } else {
+    generateGrid(gridSize);
+  }
+});

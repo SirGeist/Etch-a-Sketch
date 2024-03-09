11be1895c7f73391c8a75596be5f59851b5ccf4c
SirGeist <trinh1234533@yahoo.com>
2024-03-09 17:19:02 -0600

    Getting user input to eventually generate grid

--- myscript.js
+++ myscript.js
@@ -1,24 +1,25 @@
-// Main container
-const gridContainer = document.querySelectorAll(".grid-container");
-let size = 16;
+function generateGrid(size) {
+  // Main container
+  const gridContainer = document.querySelectorAll(".grid-container");
 
-gridContainer.forEach((gridContainer) => {
-  // Use a fragment to efficiently append all of the cells
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < size * size; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("grid-cell");
+  gridContainer.forEach((gridContainer) => {
+    // Use a fragment to efficiently append all of the cells
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < size * size; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("grid-cell");
 
-    // Change grid cell background color when mouse hovers over it
-    cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = "blue";
-    });
-    fragment.appendChild(cell);
-  }
+      // Change grid cell background color when mouse hovers over it
+      cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "blue";
+      });
+      fragment.appendChild(cell);
+    }
 
-  // Append the fragment to our container
-  gridContainer.appendChild(fragment);
+    // Append the fragment to our container
+    gridContainer.appendChild(fragment);
 
-  // Creating Grid style class for the main container
-  gridContainer.classList.add("styled-grid-container");
-});
+    // Creating Grid style class for the main container
+    gridContainer.classList.add("styled-grid-container");
+  });
+}
